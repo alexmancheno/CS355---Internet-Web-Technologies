a000a2824f9ec1e3e8e789944a2096f68f811117
alexmancheno <alex.mancheno@gmail.com>
2018-10-28 15:20:45 -0400

    Update

--- src/components/Assignment3/UserInput.js
+++ src/components/Assignment3/UserInput.js
@@ -10,8 +10,10 @@ export default class UserInput extends React.Component {
             classful: true,
             classNameValue: "A",
             numberOfHostsNeeded: 0,
+            generatedIPAddress: "ip...",
             addresses: JSON.stringify(addresses)
         }
+        localStorage.clear();
     }
 
     handleclassNameChange(event) {
@@ -19,7 +21,7 @@ export default class UserInput extends React.Component {
     }
 
     handleclassNameRadioButtonChange(bool) {
-        this.setState({classNameful: bool});
+        this.setState({classful: bool});
     }
 
     handleSubmit(event) { 
@@ -31,10 +33,6 @@ export default class UserInput extends React.Component {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
-    dec2bin(dec) {
-        return (dec >>> 0).toString(2);
-    }   
-
     generateIPv4Address() {
         console.log(this.state.addresses);
         let map;
@@ -45,37 +43,51 @@ export default class UserInput extends React.Component {
         }
         let randomAddress;
         let r = 0;
-        if (this.state.classful) {
-            if (this.state.classNameValue === "A") {
-                r += (this.getRandomInt(128)) << 24;
-            } else if (this.state.classNameValue === "B") {
-                r += (this.getRandomInt(64) + 128) << 24;
-                r += (this.getRandomInt(256) << 16);
-            } else if (this.state.classNameValue === "C") {
-                r += (this.getRandomInt(32) + 192) << 24;
-                r += (this.getRandomInt(256)) << 16;
-                r += (this.getRandomInt(256)) << 8;
-            } else if (this.state.classNameValue === "D")  {
-                r += (this.getRandomInt(16) + 224) << 24;
-                r += (this.getRandomInt(256)) << 16;
-                r += (this.getRandomInt(256)) << 8;
-                r += (this.getRandomInt(256));
-            } else if (this.state.classNameValue === "E") {
-                r += Math.ceil(Math.log2(this.state.numberOfHostsNeeded));
-                console.elog(r);
+        do {
+            if (this.state.classful) {
+                console.log("classful!");
+                if (this.state.classNameValue === "A") {
+                    r += (this.getRandomInt(128)) << 24;
+                } else if (this.state.classNameValue === "B") {
+                    r += (this.getRandomInt(64) + 128) << 24;
+                    r += (this.getRandomInt(256) << 16);
+                } else if (this.state.classNameValue === "C") {
+                    r += (this.getRandomInt(32) + 192) << 24;
+                    r += (this.getRandomInt(256)) << 16;
+                    r += (this.getRandomInt(256)) << 8;
+                } else if (this.state.classNameValue === "D")  {
+                    r += (this.getRandomInt(16) + 224) << 24;
+                    r += (this.getRandomInt(256)) << 16;
+                    r += (this.getRandomInt(256)) << 8;
+                    r += (this.getRandomInt(256));
+                } else if (this.state.classNameValue === "E") {
+                    console.log("Cannot assign class E!");
+                }
+            } else {
+                let prefixBits = Math.ceil(Math.log2(this.state.numberOfHostsNeeded));
+                r += this.getRandomInt(Math.pow(2, 32 - prefixBits));
+                r = r << (prefixBits);
             }
-        } else {
+            randomAddress = this.intToIPAddressString(r);
+        } while ((randomAddress in map)) ;
 
-        }
-
-        randomAddress = r.toString();
-        console.log(map);
-        if (!(randomAddress in map)) map[randomAddress] = "yes";
-        else console.log("Address already existed!");
-        
+        this.setState({generatedIPAddress: randomAddress});
+        map[randomAddress] = true;
         localStorage.setItem("addresses", JSON.stringify(map));
-        console.log(r.toString(2));
-        // localStorage.clear();
+        console.log(map)
+    }
+
+    intToIPAddressString(ip) {
+        let bytes = [4];
+        bytes[3] = ip & 0xFF;
+        bytes[2] = (ip >> 8) & 0xFF;
+        bytes[1] = (ip >> 16) & 0xFF;
+        bytes[0] = (ip >> 24) & 0xFF;
+        return `${bytes[0]}.${bytes[1]}.${bytes[2]}.${bytes[3]}`; 
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     handleNumberOfHostsChange(event) {
@@ -87,18 +99,18 @@ export default class UserInput extends React.Component {
             <div>
                 <form onSubmit={e => this.handleSubmit(e)}>
                     <div className="form-group-row">
-                        <legend className="col-form-label">classNameful or classNameless?</legend>
+                        <legend className="col-form-label">classful or classless?</legend>
                         <div className="radio">
                             <label>
                                 <input type="radio"
-                                    checked={this.state.classNameful === true}
+                                    checked={this.state.classful === true}
                                     onChange={e => this.handleclassNameRadioButtonChange(true)} />
                                 classful
                             </label>
                         </div>
                     <div className="form-group">
                         <label htmlFor="classNameSelect">Pick addressing className</label>
-                        <select className="form-control" id="classNameSelect" value={this.state.classNameValue} onChange={e => this.handleclassNameChange(e)}  disabled={!this.state.classNameful}>
+                        <select className="form-control" id="classNameSelect" value={this.state.classNameValue} onChange={e => this.handleclassNameChange(e)}  disabled={!this.state.classful}>
                             <option>A</option>
                             <option>B</option>
                             <option>C</option>
@@ -109,19 +121,30 @@ export default class UserInput extends React.Component {
                         <div className="radio">
                             <label>
                                 <input type="radio"
-                                    checked={this.state.classNameful === false}
+                                    checked={this.state.classful === false}
                                     onChange={e => this.handleclassNameRadioButtonChange(false)} />
                                 classless
                             </label>
                         </div>
                     <div className="input-group">
                         <span className="input-group-addon" id="basic-addon1">Number of hosts needed:</span>
-                        <input type="number" className="form-control" placeholder="hosts.." aria-describedby="basic-addon1" disabled={this.state.classNameful} onChange={e=>{this.handleNumberOfHostsChange(e)}}/>
+                        <input type="number" className="form-control" placeholder="hosts.." aria-describedby="basic-addon1" disabled={this.state.classful} onChange={e=>{this.handleNumberOfHostsChange(e)}}/>
                     </div>
                     <br/>
                     <button type="submit" value="submit" className="btn btn-primary">Assign!</button>
                     </div>
                 </form>
+            </div>
+        )
+    }
+
+    getIPAddressesDisplay() {
+        return (
+            <div>
+                <div className="form-group">
+                    <label for="exampleFormControlInput1">Your IP address:</label>
+                    <input className="form-control" type="text" name="country" value={this.state.generatedIPAddress} readOnly />
+                </div>
             </div>
         )
     }
@@ -152,6 +175,8 @@ export default class UserInput extends React.Component {
                         {this.getIPv6TabContent()}
                     </div>
                 </div>
+                <br></br>
+                {this.getIPAddressesDisplay()}
             </div>
         )
     }
